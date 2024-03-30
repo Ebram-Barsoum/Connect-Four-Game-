f495918647d3459663f58c356cf2014c415ca608
Ebram-Barsoum <abram.barsoum3.89@gmail.com>
2024-03-31 00:39:00 +0200

    optimize sound function

--- jsHub/main.js
+++ jsHub/main.js
@@ -1,8 +1,11 @@
 'use strict'
 import { game, current_player } from './game.js'
+const playSound = '../audio/play.mp3';
+const winSound = '../audio/win.mp3';
+const gameOverSound = '../audio/gameOver.mp3';
 
 const popupMsg = document.getElementById('msg'),
-     layout = document.getElementById('layout');
+    layout = document.getElementById('layout');
 
 function reset_game() {
     game.board = [
@@ -13,7 +16,6 @@ function reset_game() {
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0]
     ];
-    console.log("WTF");
     game.do.print_board();
 }
 
@@ -22,9 +24,9 @@ function reset_game() {
         for (let j = 0; j <= game.info.boardWidth; j++) {
             const row = document.querySelector(`tr:nth-child(${i + 1})`);
             const cell = row.querySelector(`td:nth-child(${j + 1}) button`);
-            
+
             cell.setAttribute('id', `${i}-${j}`)
-          
+
         }
     }
 })();
@@ -46,24 +48,24 @@ function hide_popUp() {
 const buttons = document.querySelectorAll('button');
 for (let button of buttons) {
     button.addEventListener('click', (e) => {
-       
+
 
         const [, pos_y] = e.target.getAttribute('id').split('-');
-        
-       if(game.do.drop_piece_to_bottom(pos_y)){
-            game.do.play_drop_sound();
+
+        if (game.do.drop_piece_to_bottom(pos_y)) {
+            game.do.play_sound(playSound);
             game.do.print_board();
 
             if (game.check.is_win_horizontally() || game.check.is_win_vertically() || game.check.is_win_diagonal()) {
-                game.do.play_win_sound();
+                game.do.play_sound(winSound);
                 const msg = `${game.info.currentPlayer} Won`;
                 show_popUp(msg);
-           }
-            else if (game.check.is_game_over()) {       
+            }
+            else if (game.check.is_game_over()) {
                 const msg = 'The Game Is Over !';
                 show_popUp(msg);
-                game.do.play_gameOver_sound();
-           }
+                game.do.play_sound(gameOverSound);
+            }
             game.do.change_player();
         }
     });
